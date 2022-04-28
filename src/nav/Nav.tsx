88fbf835c4fa518b3382396002d610c0f45d53c7
0xEVG <evgenikrolikov@gmail.com>
2022-04-28 19:36:24 +0700

    removed ref hook

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -1,20 +1,16 @@
 import "./Nav.scss";
 import {Props} from "../types";
 import {useRef, useState} from "react";
-import {MutableRefObject} from "react";
 
 
 export default function Nav(props:Props) {
     let [isOpen,setIsOpen] = useState(false);
-    let ref = useRef<HTMLDivElement>(null);
     function onClick() {
-        if(!ref || !ref.current) return;
-        ref.current.classList.toggle("nav__links--open");
         setIsOpen(!isOpen);
     }
     return(
         <nav className ={"nav " + props.className}>
-            <div ref={ref} className="nav__links">
+            <div className={isOpen?"nav__links nav__links--open":"nav__links"}>
                 {props.children}
             </div>
             <button onClick={onClick} className="nav__toggle_button">
